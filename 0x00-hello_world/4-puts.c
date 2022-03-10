e402b5a96bbe453d71e1ed5f8ccfc0563ad926b7
Kenneth1234 <99674811+Kenneth1234@users.noreply.github.com>
2022-03-10 17:16:45 +0300

    puts updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Dispays "Programming is like a building a multilingual puzzle
+ *
+ * Return: returns the value 0 if exited properly, otherewise non-zero
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0); /*can be ommited*/
+}
