3851b6142a459aa29cbccb16099b1e60c36da591
Kenneth1234 <99674811+Kenneth1234@users.noreply.github.com>
2022-03-15 14:52:23 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - contains the code for program execution
+ *
+ *Return: returns 0 (Success), otherwise non-zero
+ */
+int main (){
+	int c;
+
+c = getchar();
+while (c != EOF) {
+    putchar(c);
+    c = getchar();
+}
+putchar('\n');
+return (0);
+}
