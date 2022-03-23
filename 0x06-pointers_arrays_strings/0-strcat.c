4c326cff8ba90628f6bfd443e1186fa56de5a849
Kenneth1234 <kenich94@gmail.com>
2022-03-23 08:16:54 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src){
+size_t dest_len = strlen(dest);
+    size_t i;
+
+
+    for (i = 0 ; i < n && src[i] != '\0' ; i++)
+        dest[dest_len + i] = src[i];
+    dest[dest_len + i] = '\0';
+
+
+    return dest;
+
+
+
+
+
+
+}
