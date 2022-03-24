586f51dd653d947b9065e116117d19e9a6814eea
Kenneth1234 <kenich94@gmail.com>
2022-03-24 18:51:25 +0000

    t is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * infinite_add - adds two numbers
- * @n1: number to be added
- * @n2: number to be added
- * @r: the buffer that the function
- * will use to store the result
- * @size_r: the buffer size
- * Return: the pointer to dest
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -21,7 +20,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (c1 >= c2)
 		bg = c1;
 	else
-		bg = c1;
+		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
 	r[bg + 1] = '\0';
@@ -35,7 +34,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			add = 0;
 		if (op > 0)
-			*(r + bg) = (op % 10) + 48;
+		*(r + bg) = (op % 10) + 48;
 		else
 			*(r + bg) = '0';
 		if (c1 > 0)
