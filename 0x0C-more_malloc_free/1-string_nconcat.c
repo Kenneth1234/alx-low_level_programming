f4add5a7330f2b0add179948f157d1586ab5d82a
Kenneth1234 <kenich94@gmail.com>
2022-04-07 11:53:31 +0000

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,37 +6,56 @@
  * Return: lenght of a string
  */
 
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++);
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of s2
+ * Return: a pointer with the content of s1 followed by n byte of s2
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int length1, length2, i, j;
+	char *space;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (!s)
-		return (NULL);
-
-	while (i < len1)
+	if (s1 == NULL)
 	{
-		s[i] = s1[i];
-		i++;
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
 
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
+	if (n >= length2)
+	{
+		n = length2;
+	}
 
-	s[i] = '\0';
+	space = malloc(sizeof(char) * (n + length1 + 1));
 
-	return (s);
+	if (space == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0 ; i < length1 ; i++)
+		space[i] = s1[i];
+	for (j = 0 ; s2[j] != '\0' && j != n ; j++, i++)
+		space[i] = s2[j];
+
+	space[i] = '\0';
+	return (space);
 }
